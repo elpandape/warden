7893aaa3e4e1c99016a9d9791d6ecf915f9543c8
Makoto <cmayorgahilario@gmail.com>
2026-08-17 14:27:55 -0500

    chore: run rector with the php 8.4 and pest sets
    - adopt the official pest coding style set
    - keep the override attribute rector skipped

--- rector.php
+++ rector.php
@@ -2,13 +2,17 @@
 
 declare(strict_types=1);
 
+use Pest\Rector\Set\PestSetList;
 use Rector\Config\RectorConfig;
 use Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector;
 
 return RectorConfig::configure()
     ->withPaths([__DIR__.'/src', __DIR__.'/tests'])
-    ->withPhpSets(php83: true)
+    ->withPhpSets(php84: true)
     ->withPreparedSets(deadCode: true, codeQuality: true)
+    ->withSets([
+        PestSetList::CODING_STYLE,
+    ])
     ->withSkip([
         AddOverrideAttributeToOverriddenMethodsRector::class,
     ]);
